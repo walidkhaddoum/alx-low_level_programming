8e81f7a15e659412fd7ed896816ebe5426f9fa2d
Walid Khaddoum <walidkhaddoum@gmail.com>
2023-04-08 14:11:24 -0700

    style file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,9 +11,10 @@ of a number.
 Return: no return.
 */
 void print_binary(unsigned long int n) {
-  if (n > 1) {
-    print_binary(n >> 1);
-  }
+ 	if (n > 1) 
+	{
+    		print_binary(n >> 1);
+  	}
 
-  _putchar((n & 1) + '0');
+ 	 _putchar((n & 1) + '0');
 }
